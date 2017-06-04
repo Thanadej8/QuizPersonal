c29b9a7cd317a49473b1241758fe9278a4a1516a
Deklnwzaza <dietyboy@hotmail.com>
2017-06-05 02:17:19 +0700

    Create Answer Table/Controller/Model

---
+++ app/Answer.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Answer extends Model
+{
+    protected $table = 'answers';
+    protected $fillable = [
+        'user_id', 'question_id', 'answer_type', 'set'
+    ];
+}
