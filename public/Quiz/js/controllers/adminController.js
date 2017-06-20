b238731eb929bb70e98376afba169b7a65459c52
Thanadej8 <authanadej@gmail.com>
2017-06-20 18:28:47 +0700

    update

--- public/Quiz/js/controllers/adminController.js
+++ public/Quiz/js/controllers/adminController.js
@@ -25,7 +25,7 @@ app.controller('adminController',function($scope,$localStorage,$routeParams,$htt
         PersonDoQuiz.getData().then(
             function(response){
                 var data = response.data;
-
+                console.log(data);
                 data.all_user = data.all_user -1;
                 $scope.totelPerson = data;
                 console.log($scope.totelPerson);
